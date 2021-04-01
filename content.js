1404e0d2758c1fdf7ee2ae0b59ef14f6c36036c2
Dlray89 <dlrayjr89@gmail.com>
2021-04-01 12:15:30 -0400

    updated the styling for both content.js button and the popup. i use inline styling temporary. will hook up css file soon

--- content.js
+++ content.js
@@ -1,6 +1,5 @@
 console.log("Chrome extension go?");
 
-
 //Demo content
 // chrome.runtime.onMessage.addListener(gotMessage);
 
@@ -14,33 +13,44 @@ console.log("Chrome extension go?");
 //   }
 // }
 
-document.body.onload = addElement;
+document.body.onload = addButtonElement;
 
-function addElement() {
+function addButtonElement() {
   //create a new div element
   const newButton = document.createElement("button");
 
   //give the button content
-  const newContent = document.createTextNode("Click me");
+  const newContent = document.createTextNode("View claims");
+  
 
   //add the text to the button
   newButton.appendChild(newContent);
   newButton.classList.add("buttonExtension");
 
-  newButton.style.border = "solid 2px red";
+  //styling for button
+  newButton.style.background = "linear-gradient(to bottom, #2c3e50, #3498db) "
+  newButton.style.border = "solid 2px #dfdfdf";
   newButton.style.position = "relative";
-  newButton.style.top = "12em";
+  newButton.style.top = "12.5em";
   newButton.style.left = "50em";
   newButton.style.zIndex = 8000;
   newButton.style.backgroundColor = "white";
-  newButton.style.color = "red";
-  newButton.style.width = "5%";
+  newButton.style.color = "white";
+  newButton.style.width = "10%";
+  newButton.style.borderRadius = '20px'
+  newButton.style.padding = '0.5em'
+  newButton.style.boxSizing = 'border-box'
+
+
 
   const currentButton = document.getElementById("headerMain");
   document.body.insertAdjacentElement("afterbegin", newButton, currentButton);
 
-  newButton.onmouseenter = function (e) {
+  
+
+  newButton.onclick = function (e) {
     e.preventDefault();
-    window.alert("Hello there");
+    window.alert("you are current on " + document.baseURI);
+  
   };
 }
