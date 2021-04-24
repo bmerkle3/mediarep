12f0c5ced0f2a96f15bf4e2caad58b0416cf30d3
Braidy Merkle <braidy.merkle@nordstrom.com>
2021-04-23 20:51:55 -0500

    moves css to own file

--- content.js
+++ content.js
@@ -11,23 +11,21 @@ function addButtonElement() {
 
   //add the text to the button
   newButton.appendChild(newContent);
-  newButton.classList.add("buttonExtension");
+  newButton.classList.add("button");
 
   //styling for button
-  newButton.style.background = "linear-gradient(to bottom, #2c3e50, #3498db) "
-  newButton.style.border = "solid 2px #dfdfdf";
-  newButton.style.position = "relative";
-  newButton.style.top = "12.5em";
-  newButton.style.left = "50em";
-  newButton.style.zIndex = 8000;
-  newButton.style.backgroundColor = "white";
-  newButton.style.color = "white";
-  newButton.style.width = "10%";
-  newButton.style.borderRadius = '20px'
-  newButton.style.padding = '0.5em'
-  newButton.style.boxSizing = 'border-box'
-
-
+  // newButton.style.background = "linear-gradient(to bottom, #2c3e50, #3498db) ";
+  // newButton.style.border = "solid 2px #dfdfdf";
+  // newButton.style.position = "relative";
+  // newButton.style.top = "12.5em";
+  // newButton.style.left = "50em";
+  // newButton.style.zIndex = 8000;
+  // newButton.style.backgroundColor = "white";
+  // newButton.style.color = "white";
+  // newButton.style.width = "10%";
+  // newButton.style.borderRadius = "20px";
+  // newButton.style.padding = "0.5em";
+  // newButton.style.boxSizing = "border-box";
 
   const currentButton = document.getElementById("headerMain");
   document.body.insertAdjacentElement("afterbegin", newButton, currentButton);
@@ -35,6 +33,8 @@ function addButtonElement() {
   newButton.onclick = function (e) {
     e.preventDefault();
 
-    console.log("Heads up! Labeling like 'Gluten Free' has no real health benefit unless you're celiac!");
+    console.log(
+      "Heads up!! Labeling like 'Gluten Free' has no real health benefit unless you're celiac!"
+    );
   };
 }
